8743f461299ded8ac01fa25d9da4d0515ebb57c6
Karen <kxie094@aucklanduni.ac.nz>
2016-07-07 17:05:13 +1200

    Added display exercise script. tbc.

---
+++ Assets/Scripts/Menus/DisplayExercise.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class DisplayExercise : MonoBehaviour {
+
+    public BuildingSelector buildingCreator;
+    private Dictio
+
+    public UILabel label; // Instructions in text form.
+    public UISprite image; // Image shown for instruction.
+
+	// Use this for initialization
+	void Start () {
+	    if (buildingCreator != null)
+        {
+            selectedBuildingAttr = buildingCreator.GetCurrentBuildingDictionary();
+        }
+	}
+
+    public void ShowInstructionForExercise()
+    {
+        if (selectedBuildingAttr == null)
+        {
+            Debug.Log("Dictionary is null.");
+            return;
+        }
+
+        label.text = selectedBuildingAttr["TaskInstruction"];
+        image.spriteName = selectedBuildingAttr["TaskType"];
+    }
+}
