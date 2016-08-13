dfa6d38547c09a1522d8df43b13dff84eb95ebda
oratorio <frankie.lam54@hotmail.com>
2016-08-13 17:56:50 +1200

    added UserData Script

---
+++ Assets/Scripts/UserData.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserData : MonoBehaviour {
+
+    //Task stats
+    public int TasksFinished;
+    public int TimeInTasks;
+
+    //Coins
+    public int GoldEarned;
+    public int GoldSpent;
+
+    //Pets
+    public int PetsBought;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    
+}
