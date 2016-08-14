ac98e0db38b3e33fef8daec17c03298bd80f1ec4
oratorio <frankie.lam54@hotmail.com>
2016-08-14 12:04:31 +1200

    added API connection class, that posts saved data to the API

---
+++ Assets/Scripts/APIConnection.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Text;
+
+public class APIConnection : MonoBehaviour {
+
+    private string filePath;
+    private string fileName = "SOMSave";
+    private string fileExt = ".txt";
+    private string url = "http://fitsimsdata.azurewebsites.net/api/SaveDataModels";
+
+    // Use this for initialization
+    void Start () {
+        filePath = Application.persistentDataPath + "/";
+        PostData();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void PostData()
+    {
+        WWWForm form = new WWWForm();
+        StreamReader sReader = new StreamReader(filePath + fileName + fileExt);
+        //StringBuilder sBuilder = new StringBuilder();
+        string sBuilder = "";
+        while (!sReader.EndOfStream)
+        {
+            sBuilder = sBuilder+sReader.ReadLine()+" ";
+        }
+
+        form.AddField("DataBlob", sBuilder);
+        WWW www = new WWW(url, form);
+
+        StartCoroutine(SendSaveData(www));
+    }
+
+    IEnumerator SendSaveData(WWW www)
+    {
+        yield return www;
+
+         // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.text);
+        }
+        else {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+}
