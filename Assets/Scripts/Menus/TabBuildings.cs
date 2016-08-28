d51ca979063ec481c4048a055ccc61e796280515
oratorio <frankie.lam54@hotmail.com>
2016-08-28 15:03:51 +1200

    added second page to the shop screen

--- Assets/Scripts/Menus/TabBuildings.cs
+++ Assets/Scripts/Menus/TabBuildings.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class TabBuildings : MonoBehaviour {//controls the pages and arrows for buildings menu
 		
-	private const int noPages = 2;
+	private const int noPages = 3;
 	public GameObject[] Pages = new GameObject[noPages];
 	private int noPanel = 0;	
 	public GameObject ArrowLeft;
@@ -22,20 +22,31 @@ public class TabBuildings : MonoBehaviour {//controls the pages and arrows for b
 
 	public void OnArrowLeft()
 	{
-			Pages [1].SetActive (false);
-			Pages [0].SetActive (true);
-			ArrowLeft.SetActive (false);
-			ArrowRight.SetActive (true);
-			noPanel--;
+		Pages [noPanel].SetActive (false);
+		Pages [noPanel-1].SetActive (true);
+            
+        if (noPanel-1 == 0) {
+            ArrowLeft.SetActive(false);       
+        }
+
+        ArrowRight.SetActive(true);
+       
+        noPanel--;
 	}
 	
 	public void OnArrowRight()
 	{
-			Pages [0].SetActive(false);
-			Pages [1].SetActive(true);
-			ArrowRight.SetActive(false);
-			ArrowLeft.SetActive(true);
-			noPanel++;
+		Pages [noPanel].SetActive(false);
+		Pages [noPanel+1].SetActive(true);
+
+        if (noPanel+1 == noPages-1)
+        {
+            ArrowRight.SetActive(false);
+        }
+        
+        ArrowLeft.SetActive(true);
+
+        noPanel++;
 	}
 	
 }
