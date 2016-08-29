62536a10bcc56e15c1ab5826b98d4a6611c738e6
kxie094@aucklanduni.ac.nz <xie-karen@hotmail.com>
2016-08-29 18:31:38 +1200

    Remove navigbt and added autoload

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -109,7 +109,7 @@ public class MainMenu : MonoBehaviour {
 	{
 		for (int i = 0; i < InterfaceElements.Length; i++) 
 		{
-			//if(i!=9)//to disable navigation buttons
+			if(i!=6)//to disable navigation buttons
 			InterfaceElements[i].SetActive(true);
 		}
 	}
